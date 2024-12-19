3ecfffd89fdc2202df05d748c7903543075305f5
Tyler Wight <tylermwight@gmail.com>
2024-12-18 22:54:32 -0700

    convert to indices + vertices

--- src/3d_example.c
+++ src/3d_example.c
@@ -10,113 +10,70 @@
 
 float cubeVertices[] = {
     // Positions         // Colors         // Texture Coords
-    //X    //Y    //Z      //R  //G   //B      //U   //V
-
-    // plane 1 (back face)
-    -0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 0.0f,   0.0f, 0.0f,
-     0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.0f,   1.0f, 0.0f,
-     0.5f,  0.5f, -0.5f,  0.0f, 0.0f, 1.0f,   1.0f, 1.0f,
-     0.5f,  0.5f, -0.5f,  0.0f, 0.0f, 1.0f,   1.0f, 1.0f,
-    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 0.0f,   0.0f, 1.0f,
-    -0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 0.0f,   0.0f, 0.0f,
-    
-    // plane 2 (front face)
-    -0.5f, -0.5f,  0.5f,  0.0f, 1.0f, 1.0f,   0.0f, 0.0f,
-     0.5f, -0.5f,  0.5f,  1.0f, 0.0f, 1.0f,   1.0f, 0.0f,
-     0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 1.0f,   1.0f, 1.0f,
-     0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 1.0f,   1.0f, 1.0f,
-    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f, 0.0f,   0.0f, 1.0f,
-    -0.5f, -0.5f,  0.5f,  0.0f, 1.0f, 1.0f,   0.0f, 0.0f,
-
-    // plane 3 (left face)
-    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.0f,   1.0f, 0.0f,
-    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 0.0f,   1.0f, 1.0f,
-    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, 1.0f,   0.0f, 1.0f,
-    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, 1.0f,   0.0f, 1.0f,
-    -0.5f, -0.5f,  0.5f,  1.0f, 1.0f, 0.0f,   0.0f, 0.0f,
-    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.0f,   1.0f, 0.0f,
-
-    // plane 4 (right face)
-     0.5f,  0.5f,  0.5f,  0.0f, 1.0f, 1.0f,   1.0f, 0.0f,
-     0.5f,  0.5f, -0.5f,  1.0f, 0.0f, 1.0f,   1.0f, 1.0f,
-     0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 1.0f,   0.0f, 1.0f,
-     0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 1.0f,   0.0f, 1.0f,
-     0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 0.0f,   0.0f, 0.0f,
-     0.5f,  0.5f,  0.5f,  0.0f, 1.0f, 1.0f,   1.0f, 0.0f,
-
-    // plane 5 (bottom face)
-    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.0f,   0.0f, 0.0f,
-     0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 0.0f,   1.0f, 0.0f,
-     0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 1.0f,   1.0f, 1.0f,
-     0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 1.0f,   1.0f, 1.0f,
-    -0.5f, -0.5f,  0.5f,  1.0f, 1.0f, 0.0f,   0.0f, 1.0f,
-    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.0f,   0.0f, 0.0f,
-
-    // plane 6 (top face)
-    -0.5f,  0.5f, -0.5f,  1.0f, 0.0f, 1.0f,   0.0f, 0.0f,
-     0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 1.0f,   1.0f, 0.0f,
-     0.5f,  0.5f,  0.5f,  0.0f, 1.0f, 0.0f,   1.0f, 1.0f,
-     0.5f,  0.5f,  0.5f,  0.0f, 1.0f, 0.0f,   1.0f, 1.0f,
-    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.0f,   0.0f, 1.0f,
-    -0.5f,  0.5f, -0.5f,  1.0f, 0.0f, 1.0f,   0.0f, 0.0f
+    -0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 0.0f,   0.0f, 0.0f, // 0
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // 1
+     0.5f,  0.5f, -0.5f,  0.0f, 0.0f, 1.0f,   1.0f, 1.0f, // 2
+    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 0.0f,   0.0f, 1.0f, // 3
+    -0.5f, -0.5f,  0.5f,  0.0f, 1.0f, 1.0f,   0.0f, 0.0f, // 4
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f, 1.0f,   1.0f, 0.0f, // 5
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 1.0f,   1.0f, 1.0f, // 6
+    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f, 0.0f,   0.0f, 1.0f  // 7
 };
 
+unsigned int cubeIndices[] = {
+    // Back face
+    0, 1, 2,
+    2, 3, 0,
+    // Front face
+    4, 5, 6,
+    6, 7, 4,
+    // Left face
+    4, 0, 3,
+    3, 7, 4,
+    // Right face
+    1, 5, 6,
+    6, 2, 1,
+    // Bottom face
+    4, 5, 1,
+    1, 0, 4,
+    // Top face
+    3, 2, 6,
+    6, 7, 3
+};
 
 
 float planeVertices[] = {
     // Positions         // Colors         // Texture Coords
-    //X    //Y    //Z      //R  //G   //B      //U   //V
-
-    // plane 1 back
-    -5.0f, -0.5f, -10.0f,  1.0f, 0.0f, 0.0f,   0.0f, 0.0f,
-     0.5f, -0.5f, -10.0f,  0.0f, 1.0f, 0.0f,   1.0f, 0.0f,
-     0.5f,  0.1f, -10.0f,  0.0f, 0.0f, 1.0f,   1.0f, 1.0f,
-     0.5f,  0.1f, -10.0f,  0.0f, 0.0f, 1.0f,   1.0f, 1.0f,
-    -5.0f,  0.1f, -10.0f,  1.0f, 1.0f, 0.0f,   0.0f, 1.0f,
-    -5.0f, -0.5f, -10.0f,  1.0f, 0.0f, 0.0f,   0.0f, 0.0f,
-    
-    // plane 2 front
-    -5.0f, -0.5f,  0.5f,  0.0f, 1.0f, 1.0f,   0.0f, 0.0f,
-     0.5f, -0.5f,  0.5f,  1.0f, 0.0f, 1.0f,   1.0f, 0.0f,
-     0.5f,  0.1f,  0.5f,  1.0f, 1.0f, 1.0f,   1.0f, 1.0f,
-     0.5f,  0.1f,  0.5f,  1.0f, 1.0f, 1.0f,   1.0f, 1.0f,
-    -5.0f,  0.1f,  0.5f,  0.0f, 0.0f, 0.0f,   0.0f, 1.0f,
-    -5.0f, -0.5f,  0.5f,  0.0f, 1.0f, 1.0f,   0.0f, 0.0f,
-
-    // plane 3 side
-    -5.0f,  0.1f,  0.5f,  1.0f, 0.0f, 0.0f,   1.0f, 0.0f,
-    -5.0f,  0.1f, -10.0f,  0.0f, 1.0f, 0.0f,   1.0f, 1.0f,
-    -5.0f, -0.5f, -10.0f,  0.0f, 0.0f, 1.0f,   0.0f, 1.0f,
-    -5.0f, -0.5f, -10.0f,  0.0f, 0.0f, 1.0f,   0.0f, 1.0f,
-    -5.0f, -0.5f,  0.5f,  1.0f, 1.0f, 0.0f,   0.0f, 0.0f,
-    -5.0f,  0.1f,  0.5f,  1.0f, 0.0f, 0.0f,   1.0f, 0.0f,
-
-    // plane 4 side
-     0.5f,  0.1f,  0.5f,  0.0f, 1.0f, 1.0f,   1.0f, 0.0f,
-     0.5f,  0.1f, -10.0f,  1.0f, 0.0f, 1.0f,   1.0f, 1.0f,
-     0.5f, -0.5f, -10.0f,  1.0f, 1.0f, 1.0f,   0.0f, 1.0f,
-     0.5f, -0.5f, -10.0f,  1.0f, 1.0f, 1.0f,   0.0f, 1.0f,
-     0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 0.0f,   0.0f, 0.0f,
-     0.5f,  0.1f,  0.5f,  0.0f, 1.0f, 1.0f,   1.0f, 0.0f,
-
-    // plane 5 bottom
-    -5.0f, -0.5f, -10.0f,  0.0f, 1.0f, 0.0f,   0.0f, 0.0f,
-     0.5f, -0.5f, -10.0f,  1.0f, 0.0f, 0.0f,   1.0f, 0.0f,
-     0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 1.0f,   1.0f, 1.0f,
-     0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 1.0f,   1.0f, 1.0f,
-    -5.0f, -0.5f,  0.5f,  1.0f, 1.0f, 0.0f,   0.0f, 1.0f,
-    -5.0f, -0.5f, -10.0f,  0.0f, 1.0f, 0.0f,   0.0f, 0.0f,
-
-    // plane 6 top 
-    -5.0f,  0.1f, -10.0f,  1.0f, 0.0f, 1.0f,   0.0f, 0.0f,
-     0.5f,  0.1f, -10.0f,  1.0f, 1.0f, 1.0f,   1.0f, 0.0f,
-     0.5f,  0.1f,  0.5f,  0.0f, 1.0f, 0.0f,   1.0f, 1.0f,
-     0.5f,  0.1f,  0.5f,  0.0f, 1.0f, 0.0f,   1.0f, 1.0f,
-    -5.0f,  0.1f,  0.5f,  1.0f, 0.0f, 0.0f,   0.0f, 1.0f,
-    -5.0f,  0.1f, -10.0f,  1.0f, 0.0f, 1.0f,   0.0f, 0.0f
+    -5.0f, -0.5f, -10.0f,  1.0f, 0.0f, 0.0f,   0.0f, 0.0f, // 0
+     0.5f, -0.5f, -10.0f,  0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // 1
+     0.5f,  0.1f, -10.0f,  0.0f, 0.0f, 1.0f,   1.0f, 1.0f, // 2
+    -5.0f,  0.1f, -10.0f,  1.0f, 1.0f, 0.0f,   0.0f, 1.0f, // 3
+    -5.0f, -0.5f,   0.5f,  0.0f, 1.0f, 1.0f,   0.0f, 0.0f, // 4
+     0.5f, -0.5f,   0.5f,  1.0f, 0.0f, 1.0f,   1.0f, 0.0f, // 5
+     0.5f,  0.1f,   0.5f,  1.0f, 1.0f, 1.0f,   1.0f, 1.0f, // 6
+    -5.0f,  0.1f,   0.5f,  0.0f, 0.0f, 0.0f,   0.0f, 1.0f  // 7
 };
 
-
+unsigned int planeIndices[] = {
+    // Back face
+    0, 1, 2,
+    2, 3, 0,
+    // Front face
+    4, 5, 6,
+    6, 7, 4,
+    // Left face
+    4, 0, 3,
+    3, 7, 4,
+    // Right face
+    1, 5, 6,
+    6, 2, 1,
+    // Bottom face
+    4, 5, 1,
+    1, 0, 4,
+    // Top face
+    3, 2, 6,
+    6, 7, 3
+};
 
 
 
@@ -150,18 +107,25 @@ int main() {
     GLuint shaderProgram = createShaderProgram("shaders/3d_ex_vertex_shader.glsl", "shaders/3d_ex_fragment_shader.glsl");
     glUseProgram(shaderProgram);
     
-
+    //////////
     // VAOVBO 1
+    /////////
     GLuint VAO[2];
     GLuint VBO[2];
+    GLuint EBO[2];
+
     glGenVertexArrays(1, &VAO[0]);
     glGenBuffers(1, &VBO[0]);
+    glGenBuffers(1, &EBO[0]);
 
     glBindVertexArray(VAO[0]);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[0]);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cubeIndices), cubeIndices, GL_STATIC_DRAW);
+
     // Position attribute
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
@@ -175,15 +139,22 @@ int main() {
     glEnableVertexAttribArray(2);
 
 
+
+    ///////////
     //VAO VBO 2
+    //////////
     glGenVertexArrays(1, &VAO[1]);
     glGenBuffers(1, &VBO[1]);
+    glGenBuffers(1, &EBO[1]);
 
     glBindVertexArray(VAO[1]);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertices), planeVertices, GL_STATIC_DRAW);
 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[0]);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(planeIndices), planeIndices, GL_STATIC_DRAW);
+
     // Position attribute
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
@@ -197,43 +168,45 @@ int main() {
     glEnableVertexAttribArray(2);
 
 
+
+
     //load textures
     GLuint texture1 = createTexture("assets/brick.png");
 
 
+    //setup mvp
+
+    // Model matrix
+    mat4 cubeModel = GLM_MAT4_IDENTITY_INIT;
+
+    mat4 planeModel = GLM_MAT4_IDENTITY_INIT;
+    glm_translate(planeModel, (vec3){-5.0f, 0.0f, 0.0f});
+
+    // View matrix
+    mat4 view = GLM_MAT4_IDENTITY_INIT;
+    glm_translate(view, (vec3){0.0f, 0.0f, -100.0f}); // set camera some units back in Z from the cube
+
+    // Projection matrix
+    mat4 projection;
+    glm_perspective(glm_rad(45.0f), 800.0f / 600.0f, 0.1f, 100.0f, projection); // set FOV, Aspect Ratio, and near and far plane on screen
+
+
+
     glEnable(GL_DEPTH_TEST);
     glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-     float spin = 0;
+    glUseProgram(shaderProgram);
+    float spin = 0.005;
     // Main loop
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+        // rotate cube
+        glm_rotate(cubeModel, spin, (vec3){0.5f, 1.0f, 0.0f});
 
-        // Use shader program
-        glUseProgram(shaderProgram);
+        processInput(window, cubeModel, view, projection);
 
-        // Model matrix
-        mat4 model = GLM_MAT4_IDENTITY_INIT;
-        glm_rotate(model, spin, (vec3){0.5f, 1.0f, 0.0f});
-
-        mat4 model2 = GLM_MAT4_IDENTITY_INIT;
-        glm_translate(model2, (vec3){-5.0f, 0.0f, 0.0f});
-        //glm_rotate(model2, (spin * -1.0f), (vec3){0.5f, 1.0f, 0.0f});
-
-        spin += 0.005;
-        //printf("time: %f\n", (float)glfwGetTime());
-
-        // View matrix
-        mat4 view = GLM_MAT4_IDENTITY_INIT;
-        glm_translate(view, (vec3){0.0f, 0.0f, -100.0f}); // set camera some units back in Z from the cube
-
-        // Projection matrix
-        mat4 projection;
-        glm_perspective(glm_rad(45.0f), 800.0f / 600.0f, 0.1f, 100.0f, projection); // set FOV, Aspect Ratio, and near and far plane on screen
-
-        processInput(window, model, view, projection);
-
-        // pas MVP to shader as uniforms
-        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "model"), 1, GL_FALSE, (const GLfloat *)model);
+        // pass MVP to shader as uniforms
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "model"), 1, GL_FALSE, (const GLfloat *)cubeModel);
         glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "view"), 1, GL_FALSE, (const GLfloat *)view);
         glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "projection"), 1, GL_FALSE, (const GLfloat *)projection);
         
@@ -245,12 +218,15 @@ int main() {
 
         // Draw cube
         glBindVertexArray(VAO[0]);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 
         //Draw Plane
-        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "model"), 1, GL_FALSE, (const GLfloat *)model2);
+        glUniformMatrix4fv(glGetUniformLocation(shaderProgram, "model"), 1, GL_FALSE, (const GLfloat *)planeModel);
         glBindVertexArray(VAO[1]);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+        
+        
         // Swap buffers and poll IO events
         glfwSwapBuffers(window);
         glfwPollEvents();
