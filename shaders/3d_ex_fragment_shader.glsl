cad38015fe9d1960040a97f2901e7dc379cca545
Tyler Wight <tylermwight@gmail.com>
2024-12-18 22:31:24 -0700

    cleanup

---
+++ shaders/3d_ex_fragment_shader.glsl
@@ -0,0 +1,12 @@
+#version 330 core
+
+in vec3 vertexColor;
+in vec2 texCoord;
+
+out vec4 fragColor;
+
+uniform sampler2D texture1;
+
+void main() {
+    fragColor = texture(texture1, texCoord);
+}
--- src/3d_example.c
+++ src/3d_example.c
@@ -5,60 +5,8 @@
 #include <cglm/cglm.h>
 #define STB_IMAGE_IMPLEMENTATION
 #include "../include/stb_image.h"
+#include "opengl_helpers.c"
 
-
-// float cubeVertices[] = {
-//     // Positions         // Colors
-//     //X    //Y    //Z      //R  //G   //B
-
-//     // plane 1
-//     -0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 0.0f,
-//      0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
-//      0.5f,  0.5f, -0.5f,  0.0f, 0.0f, 1.0f,
-//      0.5f,  0.5f, -0.5f,  0.0f, 0.0f, 1.0f,
-//     -0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 0.0f,
-//     -0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 0.0f,
-    
-//     // plane 2
-//     -0.5f, -0.5f,  0.5f,  0.0f, 1.0f, 1.0f,
-//      0.5f, -0.5f,  0.5f,  1.0f, 0.0f, 1.0f,
-//      0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 1.0f,
-//      0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 1.0f,
-//     -0.5f,  0.5f,  0.5f,  0.0f, 0.0f, 0.0f,
-//     -0.5f, -0.5f,  0.5f,  0.0f, 1.0f, 1.0f,
-
-//     // plane 3
-//     -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.0f,
-//     -0.5f,  0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
-//     -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, 1.0f,
-//     -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, 1.0f,
-//     -0.5f, -0.5f,  0.5f,  1.0f, 1.0f, 0.0f,
-//     -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.0f,
-
-//     // plane 4
-//      0.5f,  0.5f,  0.5f,  0.0f, 1.0f, 1.0f,
-//      0.5f,  0.5f, -0.5f,  1.0f, 0.0f, 1.0f,
-//      0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 1.0f,
-//      0.5f, -0.5f, -0.5f,  1.0f, 1.0f, 1.0f,
-//      0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 0.0f,
-//      0.5f,  0.5f,  0.5f,  0.0f, 1.0f, 1.0f,
-
-//     // plane 5
-//     -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
-//      0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 0.0f,
-//      0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 1.0f,
-//      0.5f, -0.5f,  0.5f,  0.0f, 0.0f, 1.0f,
-//     -0.5f, -0.5f,  0.5f,  1.0f, 1.0f, 0.0f,
-//     -0.5f, -0.5f, -0.5f,  0.0f, 1.0f, 0.0f,
-
-//     // plane 6
-//     -0.5f,  0.5f, -0.5f,  1.0f, 0.0f, 1.0f,
-//      0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 1.0f,
-//      0.5f,  0.5f,  0.5f,  0.0f, 1.0f, 0.0f,
-//      0.5f,  0.5f,  0.5f,  0.0f, 1.0f, 0.0f,
-//     -0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.0f,
-//     -0.5f,  0.5f, -0.5f,  1.0f, 0.0f, 1.0f
-// };
 
 float cubeVertices[] = {
     // Positions         // Colors         // Texture Coords
@@ -170,157 +118,8 @@ float planeVertices[] = {
 
 
 
-// shaders
-const char *vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"layout (location = 1) in vec3 aColor;\n"
-"layout (location = 2) in vec2 aTexCoord;\n" // Texture coordinate input
-"out vec3 vertexColor;\n"
-"out vec2 texCoord;\n" // Pass texture coordinates to the fragment shader
-"uniform mat4 model;\n"
-"uniform mat4 view;\n"
-"uniform mat4 projection;\n"
-"void main() {\n"
-"    gl_Position = projection * view * model * vec4(aPos, 1.0);\n"
-"    vertexColor = aColor;\n"
-"    texCoord = aTexCoord;\n" // Pass the texture coordinates
-"}\n";
 
 
-const char *fragmentShaderSource = "#version 330 core\n"
-"in vec3 vertexColor;\n"
-"in vec2 texCoord;\n" // Receive texture coordinates from the vertex shader
-"out vec4 fragColor;\n"
-"uniform sampler2D texture1;\n"
-"void main() {\n"
-"    fragColor = texture(texture1, texCoord);\n" // Sample the texture using texCoord
-"}\n";
-
-
-// void processInput(GLFWwindow *window, mat4 *model, mat4 *view, mat4 *projection) {
-//     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-//         glfwSetWindowShouldClose(window, 1);
-// }
-
-void processInput(GLFWwindow *window, mat4 *model, mat4 *view, mat4 *projection) {
-    static vec3 cameraPos = {-2.0f, 0.0f, 10.0f};  // Initial camera position
-    static vec3 cameraFront = {0.0f, 0.0f, -1.0f}; // Camera forward direction
-    static vec3 cameraUp = {0.0f, 1.0f, 0.0f};    // Camera up direction
-
-    static float yaw = -90.0f;  // Initial yaw (pointing along -Z)
-    static float pitch = 0.0f;  // Initial pitch
-    static float lastX = 400.0f; // Last mouse X position (assume 800x600 window)
-    static float lastY = 300.0f; // Last mouse Y position
-    static int firstMouse = 1;  // To handle first mouse movement
-
-    float cameraSpeed = 0.025f;   // Adjust for movement speed
-    float mouseSensitivity = 0.2f;  // Adjust for mouse sensitivity
-
-    // Handle ESC key
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, 1);
-
-    // WASD input for movement
-    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
-        vec3 scaledFront;
-        glm_vec3_scale(cameraFront, cameraSpeed, scaledFront);
-        glm_vec3_add(cameraPos, scaledFront, cameraPos);
-    }
-    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
-        vec3 scaledFront;
-        glm_vec3_scale(cameraFront, cameraSpeed, scaledFront);
-        glm_vec3_sub(cameraPos, scaledFront, cameraPos);
-    }
-    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
-        vec3 cameraRight;
-        glm_vec3_cross(cameraFront, cameraUp, cameraRight);
-        glm_vec3_normalize(cameraRight);
-        glm_vec3_scale(cameraRight, cameraSpeed, cameraRight);
-        glm_vec3_sub(cameraPos, cameraRight, cameraPos);
-    }
-    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
-        vec3 cameraRight;
-        glm_vec3_cross(cameraFront, cameraUp, cameraRight);
-        glm_vec3_normalize(cameraRight);
-        glm_vec3_scale(cameraRight, cameraSpeed, cameraRight);
-        glm_vec3_add(cameraPos, cameraRight, cameraPos);
-    }
-
-    // Mouse input for camera direction
-    double xpos, ypos;
-    glfwGetCursorPos(window, &xpos, &ypos);
-
-    if (firstMouse) {
-        lastX = xpos;
-        lastY = ypos;
-        firstMouse = 0;
-    }
-
-    float xOffset = xpos - lastX;
-    float yOffset = lastY - ypos;  // Reversed since Y-coordinates range bottom to top
-    lastX = xpos;
-    lastY = ypos;
-
-    xOffset *= mouseSensitivity;
-    yOffset *= mouseSensitivity;
-
-    yaw += xOffset;
-    pitch += yOffset;
-
-    // Constrain the pitch angle to prevent flipping
-    if (pitch > 89.0f)
-        pitch = 89.0f;
-    if (pitch < -89.0f)
-        pitch = -89.0f;
-
-    // Update camera front vector based on yaw and pitch
-    vec3 front;
-    front[0] = cosf(glm_rad(yaw)) * cosf(glm_rad(pitch));
-    front[1] = sinf(glm_rad(pitch));
-    front[2] = sinf(glm_rad(yaw)) * cosf(glm_rad(pitch));
-    glm_vec3_normalize(front);
-    glm_vec3_copy(front, cameraFront);
-
-    // Update the view matrix
-    glm_lookat(cameraPos, (vec3){cameraPos[0] + cameraFront[0], cameraPos[1] + cameraFront[1], cameraPos[2] + cameraFront[2]}, cameraUp, *view);
-}
-
-GLuint createTexture(const char *filePath) {
-    GLuint textureID;
-    glGenTextures(1, &textureID);
-    glBindTexture(GL_TEXTURE_2D, textureID);
-
-    // Set texture wrapping and filtering options
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); // Wrap horizontally
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT); // Wrap vertically
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR); // Minification filter
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); // Magnification filter
-
-    // Load image using stb_image
-    int width, height, nrChannels;
-    stbi_set_flip_vertically_on_load(1); // Flip images vertically (most image formats are stored upside down for OpenGL)
-    unsigned char *data = stbi_load(filePath, &width, &height, &nrChannels, 0);
-    if (data) {
-        GLenum format;
-        if (nrChannels == 1)
-            format = GL_RED;
-        else if (nrChannels == 3)
-            format = GL_RGB;
-        else if (nrChannels == 4)
-            format = GL_RGBA;
-
-        // Load texture data into OpenGL
-        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    } else {
-        fprintf(stderr, "Failed to load texture: %s\n", filePath);
-        stbi_image_free(data);
-        return 0; // Return 0 for failed texture loading
-    }
-
-    stbi_image_free(data);
-    return textureID;
-}
 
 
 int main() {
@@ -347,45 +146,12 @@ int main() {
         return -1;
     }
 
-    //compile and link shaders
-    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
+    //load shaders
+    GLuint shaderProgram = createShaderProgram("shaders/3d_ex_vertex_shader.glsl", "shaders/3d_ex_fragment_shader.glsl");
+    glUseProgram(shaderProgram);
+    
 
-    int success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        fprintf(stderr, "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n%s\n", infoLog);
-    }
-
-    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        fprintf(stderr, "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n%s\n", infoLog);
-    }
-
-    GLuint shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        fprintf(stderr, "ERROR::SHADER::PROGRAM::LINKING_FAILED\n%s\n", infoLog);
-    }
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
-
-
-    // Create VAO and VBO
+    // VAOVBO 1
     GLuint VAO[2];
     GLuint VBO[2];
     glGenVertexArrays(1, &VAO[0]);
@@ -431,22 +197,15 @@ int main() {
     glEnableVertexAttribArray(2);
 
 
-
-    // Enable depth testing
-    glEnable(GL_DEPTH_TEST);
-    //glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-
-    float spin = 0;
-
-    //load texturse
+    //load textures
     GLuint texture1 = createTexture("assets/brick.png");
+
+
+    glEnable(GL_DEPTH_TEST);
     glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+     float spin = 0;
     // Main loop
     while (!glfwWindowShouldClose(window)) {
-        // Input
-        
-
-        // Render
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // Use shader program
